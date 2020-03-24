18b33c7ce4ca8002f001643312e06e4aebd1f2f0
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2020-03-24 12:55:09 +0000

    Restructured into functions.

--- main.cpp
+++ main.cpp
@@ -6,12 +6,26 @@ GLFWwindow *window;
 
 void initWindow()
 {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL 
+    window = glfwCreateWindow(500, 500, "Simulation", nullptr, nullptr);
+    if (window == NULL)
+    {
+        throw std::runtime_error("Failed to open GLFW window.");
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 }
 
 void mainLoop()
 {
     while(!glfwWindowShouldClose(window))
     {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 }
 
@@ -32,28 +46,5 @@ int main()
 {
     std::cout << "Hello, world!" << std::endl;
 
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL 
-    window = glfwCreateWindow(500, 500, "Simulation", nullptr, nullptr);
-    if (window == NULL)
-    {
-        throw std::runtime_error("Failed to open GLFW window.");
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
-    do{
-
-        // Draw nothing, see you in tutorial 2 !
-
-        // Swap buffers
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-
-    } // Check if the ESC key was pressed or the window was closed
-    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-        glfwWindowShouldClose(window) == 0 );
+    run();
 }
