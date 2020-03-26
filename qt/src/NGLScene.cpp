b5752f99418792730c3f3b865c87992561b67a6d
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2020-03-26 09:34:19 +0000

    Working 2D grid with lines.

--- qt/src/NGLScene.cpp
+++ qt/src/NGLScene.cpp
@@ -13,7 +13,7 @@
 #include <ngl/NGLStream.h>
 
 constexpr float gridSize=1.5;
-constexpr int steps=100;
+constexpr int steps=10;
 constexpr auto shaderProgram = "Grid";
 
 NGLScene::NGLScene()
@@ -117,8 +117,6 @@ void NGLScene::loadMatricesToShader(const std::string& shaderName)
   t.normalMatrix=t.M;
   t.normalMatrix.inverse().transpose();
   shader->setUniformBuffer("TransformUBO",sizeof(transform),&t.MVP.m_00);
-
-  std::cout << "Transform: \n" << t.MVP << std::endl;
 }
 
 void  NGLScene::makeGrid()
@@ -126,7 +124,7 @@ void  NGLScene::makeGrid()
   GLfloat _size=gridSize;
   size_t _steps = steps;
   m_vboSize= (_steps+2)*12;
-  std::unique_ptr<GLfloat []>vertexData( new GLfloat[m_vboSize]);
+  std::unique_ptr<GLfloat []>vertexData(new GLfloat[m_vboSize]);
 
   float step=_size/static_cast<float>(_steps);
 	float s2=_size/2.0f;
@@ -134,10 +132,10 @@ void  NGLScene::makeGrid()
 
   for(size_t i=0; i<=_steps; ++i)
 	{
-    m_gridVBO.push_back({-s2, v, 0.0f});
-    m_gridVBO.push_back({s2, v, 0.0f});
-    m_gridVBO.push_back({v, s2, 0.0f});
-    m_gridVBO.push_back({v, -s2, 0.0f});
+    m_gridVBO.push_back({-s2, v, 0.0f}); // Left vert
+    m_gridVBO.push_back({s2, v, 0.0f});  // Right vert
+    m_gridVBO.push_back({v, s2, 0.0f});  // Top vert
+    m_gridVBO.push_back({v, -s2, 0.0f}); // Bottom vert
 
 		v+=step;
 	}
@@ -150,8 +148,12 @@ void  NGLScene::makeGrid()
   m_gridVAO->setData(ngl::SimpleVAO::VertexData(size*sizeof(ngl::Vec3), m_gridVBO[0].m_x));
   m_gridVAO->setNumIndices(size);
   m_gridVAO->setVertexAttributePointer(0,3,GL_FLOAT,0,0);
+
   glPointSize(5);
-  m_gridVAO->setMode(GL_POINTS);
+  glEnable(GL_LINE_SMOOTH);
+  glLineWidth(50);
+
+  m_gridVAO->setMode(GL_LINES);
 
   m_gridVAO->unbind();
 }
