ed5a0b59d6227b4edd2ed04a3c84f4db7f5637ba
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2020-03-26 14:14:53 +0000

    Added point shaders.

---
+++ qt/shaders/PointFragment.glsl
@@ -0,0 +1,8 @@
+#version 330 core
+// This code is based on code from here https://learnopengl.com/#!PBR/Lighting
+layout (location =0) out vec4 fragColour;
+
+void main()
+{
+    fragColour = vec4(0.0f, 0.5f, 0.6f, 1.0);
+}
---
+++ qt/shaders/PointVertex.glsl
@@ -0,0 +1,21 @@
+#version 410 core
+// this demo is based on code from here https://learnopengl.com/#!PBR/Lighting
+/// @brief the vertex passed in
+layout (location = 0) in vec3 inVert;
+/// @brief the normal passed in
+layout (location = 1) in vec3 inNormal;
+
+out vec3 worldPos;
+out vec3 normal;
+
+layout(std140) uniform TransformUBO
+{
+  mat4 MVP;
+  mat4 normalMatrix;
+  mat4 M;
+}transforms;
+
+void main()
+{
+    gl_Position = transforms.MVP*vec4(inVert,1.0);
+}
