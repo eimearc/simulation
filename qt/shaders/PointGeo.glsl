019754269e15a21b48bedf458f17812e2d51ae06
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2020-03-26 15:27:47 +0000

    Made normals smaller.

--- qt/shaders/PointGeo.glsl
+++ qt/shaders/PointGeo.glsl
@@ -4,6 +4,8 @@ layout(line_strip, max_vertices = 2) out;
 
 in vec4 normal[];
 
+uniform float size;
+
 out vec3 colour;
 
 void main()
@@ -13,7 +15,7 @@ void main()
     gl_Position = gl_in[0].gl_Position;
     EmitVertex();
 
-    gl_Position = gl_in[0].gl_Position + normal[0];
+    gl_Position = gl_in[0].gl_Position + (normal[0]*0.025f);
     EmitVertex();
 
     EndPrimitive();
